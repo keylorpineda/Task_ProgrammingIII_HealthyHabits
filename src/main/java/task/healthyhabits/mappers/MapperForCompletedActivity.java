c06dcce2d51e27cceb8093eb07d886777de1ba96
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2025-08-15 21:43:34 -0600

    [FIX] fix mappers for entities

--- src/main/java/task/healthyhabits/mappers/MapperForCompletedActivity.java
+++ src/main/java/task/healthyhabits/mappers/MapperForCompletedActivity.java
@@ -1,6 +1,7 @@
 package task.healthyhabits.mappers;
 
 import task.healthyhabits.models.CompletedActivity;
+import task.healthyhabits.models.Habit;
 import task.healthyhabits.dtos.inputs.CompletedActivityInputDTO;
 import task.healthyhabits.dtos.outputs.CompletedActivityOutputDTO;
 import task.healthyhabits.dtos.normals.CompletedActivityDTO;
@@ -8,49 +9,56 @@ import task.healthyhabits.dtos.normals.CompletedActivityDTO;
 public class MapperForCompletedActivity {
 
     public static CompletedActivity toModel(CompletedActivityInputDTO input) {
-        if (input == null) return null;
-
+        if (input == null)
+            return null;
         CompletedActivity ca = new CompletedActivity();
-        ca.setHabit(MapperForHabit.toModel(input.getHabit()));
+        ca.setHabit(refHabit(input.getHabitId()));
         ca.setCompletedAt(input.getCompletedAt());
         ca.setNotes(input.getNotes());
-        ca.setProgressLog(MapperForProgressLog.toModel(input.getProgressLog()));
         return ca;
     }
 
     public static CompletedActivityOutputDTO toOutput(CompletedActivity model) {
-        if (model == null) return null;
-
+        if (model == null)
+            return null;
         return new CompletedActivityOutputDTO(
-            model.getId(),
-            MapperForHabit.toOutput(model.getHabit()),
-            model.getCompletedAt(),
-            model.getNotes(),
-            MapperForProgressLog.toOutput(model.getProgressLog())
-        );
+                model.getId(),
+                MapperForHabit.toOutput(model.getHabit()),
+                model.getCompletedAt(),
+                model.getNotes(),
+                null);
     }
 
     public static CompletedActivityDTO toDTO(CompletedActivity model) {
-        if (model == null) return null;
-
+        if (model == null)
+            return null;
         return new CompletedActivityDTO(
-            model.getId(),
-            MapperForHabit.toDTO(model.getHabit()),
-            model.getCompletedAt(),
-            model.getNotes(),
-            MapperForProgressLog.toDTO(model.getProgressLog())
-        );
+                model.getId(),
+                MapperForHabit.toDTO(model.getHabit()),
+                model.getCompletedAt(),
+                model.getNotes(),
+                null);
     }
 
     public static CompletedActivity fromDTO(CompletedActivityDTO dto) {
-        if (dto == null) return null;
-
+        if (dto == null)
+            return null;
         CompletedActivity ca = new CompletedActivity();
         ca.setId(dto.getId());
         ca.setHabit(MapperForHabit.fromDTO(dto.getHabit()));
         ca.setCompletedAt(dto.getCompletedAt());
         ca.setNotes(dto.getNotes());
-        ca.setProgressLog(MapperForProgressLog.fromDTO(dto.getProgressLog()));
+        if (dto.getProgressLog() != null) {
+            ca.setProgressLog(MapperForProgressLog.fromDTO(dto.getProgressLog()));
+        }
         return ca;
+    }
+
+    private static Habit refHabit(Long id) {
+        if (id == null)
+            return null;
+        Habit h = new Habit();
+        h.setId(id);
+        return h;
     }
 }
--- src/main/java/task/healthyhabits/mappers/MapperForRoutineActivity.java
+++ src/main/java/task/healthyhabits/mappers/MapperForRoutineActivity.java
@@ -1,6 +1,7 @@
 package task.healthyhabits.mappers;
 
 import task.healthyhabits.models.RoutineActivity;
+import task.healthyhabits.models.Habit;
 import task.healthyhabits.dtos.inputs.RoutineActivityInputDTO;
 import task.healthyhabits.dtos.outputs.RoutineActivityOutputDTO;
 import task.healthyhabits.dtos.normals.RoutineActivityDTO;
@@ -9,54 +10,53 @@ public class MapperForRoutineActivity {
 
     public static RoutineActivity toModel(RoutineActivityInputDTO input) {
         if (input == null) return null;
-
         RoutineActivity activity = new RoutineActivity();
-        activity.setHabit(MapperForHabit.toModel(input.getHabit()));
+        activity.setHabit(refHabit(input.getHabitId()));
         activity.setDuration(input.getDuration());
         activity.setTargetTime(input.getTargetTime());
         activity.setNotes(input.getNotes());
-        activity.setRoutine(MapperForRoutine.toModel(input.getRoutine()));
-
         return activity;
     }
 
     public static RoutineActivityOutputDTO toOutput(RoutineActivity model) {
         if (model == null) return null;
-
         return new RoutineActivityOutputDTO(
             model.getId(),
             MapperForHabit.toOutput(model.getHabit()),
             model.getDuration(),
             model.getTargetTime(),
             model.getNotes(),
-            MapperForRoutine.toOutput(model.getRoutine())
+            null
         );
     }
 
     public static RoutineActivityDTO toDTO(RoutineActivity model) {
         if (model == null) return null;
-
         return new RoutineActivityDTO(
             model.getId(),
             MapperForHabit.toDTO(model.getHabit()),
             model.getDuration(),
             model.getTargetTime(),
             model.getNotes(),
-            MapperForRoutine.toDTO(model.getRoutine())
+            null
         );
     }
 
     public static RoutineActivity fromDTO(RoutineActivityDTO dto) {
         if (dto == null) return null;
-
         RoutineActivity activity = new RoutineActivity();
         activity.setId(dto.getId());
         activity.setHabit(MapperForHabit.fromDTO(dto.getHabit()));
         activity.setDuration(dto.getDuration());
         activity.setTargetTime(dto.getTargetTime());
         activity.setNotes(dto.getNotes());
-        activity.setRoutine(MapperForRoutine.fromDTO(dto.getRoutine()));
-
         return activity;
+    }
+
+    private static Habit refHabit(Long id) {
+        if (id == null) return null;
+        Habit h = new Habit();
+        h.setId(id);
+        return h;
     }
 }
