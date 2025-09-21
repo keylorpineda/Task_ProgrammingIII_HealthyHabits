5cb7f03f498f87988575178780cf318956b83c5d
keylorpineda <keylor.pineda.alvarez@est.una.ac.cr>
2025-09-21 15:37:11 -0600

    Refactor AdminInitializer to log warnings instead of throwing exceptions for missing admin password configurations

--- src/test/java/task/healthyhabits/configTest/AdminInitializerTest.java
+++ src/test/java/task/healthyhabits/configTest/AdminInitializerTest.java
@@ -3,11 +3,16 @@ package task.healthyhabits.configTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.Optional;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -22,7 +27,9 @@ import task.healthyhabits.models.Role;
 import task.healthyhabits.models.User;
 import task.healthyhabits.repositories.RoleRepository;
 import task.healthyhabits.repositories.UserRepository;
+import org.springframework.core.env.Environment;
 import task.healthyhabits.security.hash.PasswordHashService;
+import org.slf4j.LoggerFactory;
 
 @ExtendWith(MockitoExtension.class)
 class AdminInitializerTest {
@@ -36,6 +43,9 @@ class AdminInitializerTest {
     @Mock
     private PasswordHashService passwordHashService;
 
+    @Mock
+    private Environment environment;
+
     @InjectMocks
     private AdminInitializer initializer;
 
@@ -46,7 +56,8 @@ class AdminInitializerTest {
     @BeforeEach
     void configureProperties() {
         ReflectionTestUtils.setField(initializer, "adminEmail", adminEmail);
-        ReflectionTestUtils.setField(initializer, "adminPassword", rawPassword);
+        when(environment.getProperty("app.admin.password")).thenReturn(rawPassword);
+        when(environment.getProperty("APP_ADMIN_PASSWORD")).thenReturn(rawPassword);
     }
 
     @Test
@@ -89,5 +100,32 @@ class AdminInitializerTest {
         assertThat(admin.getEmail()).isEqualTo(adminEmail);
         assertThat(admin.getPassword()).isEqualTo(encodedPassword);
         assertThat(admin.getRoles()).containsExactlyElementsOf(roles);
+    }
+
+    @Test
+    void runLogsWarningAndSkipsWhenPasswordMissing() {
+        when(environment.getProperty("app.admin.password")).thenReturn("");
+        when(environment.getProperty("APP_ADMIN_PASSWORD")).thenReturn(null);
+
+        Logger logger = (Logger) LoggerFactory.getLogger(AdminInitializer.class);
+        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
+        listAppender.start();
+        logger.addAppender(listAppender);
+
+        try {
+            initializer.run();
+        } finally {
+            logger.detachAppender(listAppender);
+        }
+
+        verifyNoInteractions(userRepository, roleRepository, passwordHashService);
+
+        assertThat(listAppender.list)
+                .anySatisfy(event -> {
+                    assertThat(event.getLevel()).isEqualTo(Level.WARN);
+                    assertThat(event.getFormattedMessage())
+                            .contains("Administrator account seeding skipped")
+                            .contains("APP_ADMIN_PASSWORD");
+                });
     }
 }
